0416593aec8f1b7a3d05adfeb3279008afdb035b
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2024-10-06 16:46:48 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const totalJogos = document.querySelector('#totalJogos').value;
+const qtdPontos = document.querySelector('#qtdPontos').value;
+const pontosPossiveis = document.querySelector('#qtdPontos').value;
+
+const Resultado = (qtdPontos/pontosPossiveis)*100;
+
+pontosPossiveis = totalJogos*3;
+
+
+function executa(){
+    alert(dadosForm)
+
+}
+
+
